09268d3b8e8faec7cab1986744eddc07ff5be88a
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-09-01 20:16:57 +0200

    Init project

---
+++ src/test/java/fr/hb/mlang/electricitybusiness/ElectricityBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hb.mlang.electricitybusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBusinessApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
