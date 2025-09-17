23801481fc47b830f86ade3e36c8aa40e7c5acaf
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-09-17 07:45:02 +0200

    Feat: init entities repositories

---
+++ src/main/java/fr/hb/mlang/electricitybusiness/modules/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package fr.hb.mlang.electricitybusiness.modules.user.repository;
+
+import fr.hb.mlang.electricitybusiness.modules.user.domain.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByEmailOrPhoneNumber(String email, String phoneNumber);
+}
