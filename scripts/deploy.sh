86b6119534f4a3307532711314e8a9e560a4ac87
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-09-25 17:46:44 +0200

    Chore: continued deployment guide & added rollback script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,10 @@ BRANCH="${BRANCH:-main}"
 PROFILE="${PROFILE:-prod}"
 
 # Deployment steps
-echo "[0/6] Starting deployment procedure..."
+echo "Starting deployment procedure..."
 
-echo "[1/5] Checking project directory..."
+# Checking if project exists or cloning repository
+echo "[STEP-1] Checking project directory..."
 if [[ ! -d "$APP_DIR/.git" ]]; then
   echo "Project doesn't exist. Cloning '$REPO_URL' into '$APP_DIR' ..."
   git clone "$REPO_URL" "$APP_DIR"
@@ -22,25 +23,35 @@ fi
 
 cd "$APP_DIR"
 
-echo "[2/5] Switching to branch '$BRANCH' and pulling latest changes..."
+# Pull latest changes
+echo "[STEP-2] Switching to branch '$BRANCH' and pulling latest changes..."
 git switch "$BRANCH"
 git pull --ff-only
 
-echo "[3/5] Installing dependencies & running tests..."
+# Install dependencies & automatically run tests
+echo "[STEP-3] Installing dependencies & running tests..."
 ./mvnw -B test
-echo "[3/5] Tests passed."
+echo "Tests passed."
 
-echo "[4/5] Packaging artifact (skip tests)..."
+# Package the application
+echo "[STEP-4] Packaging artifact (skip tests)..."
 ./mvnw -B -DskipTests package
 
-echo "[5/5] Run the application JAR with profile=$PROFILE..."
-#mvn -q spring-boot-run -Dspring-boot.run.profiles="${PROFILE}"
-JAR_PATH="$(ls -1 target/*.jar | head -n1)"
-if [[ -z "${JAR_PATH}" ]]; then
-  echo "No JAR found in 'target/.'. Check packaging step." >&2
-  exit 1
-fi
+# Store new JAR file in /releases folder and names it after timestamp
+echo "[STEP-5] "
+mkdir -p releases
+NEW_JAR="$(ls -1 target/*.jar | head -n1)"
+STAMP="$(date+%Y%m%d-%H%M%S)"
+cp "$NEW_JAR" "releases/app-$STAMP}.jar"
 
-echo "Starting: java -jar \"$JAR_PATH\" --spring.profiles.active=$PROFILE"
+# Shift symlink
+if [[ -L releases/current.jar ]]; then
+  OLD_TARGET="$(readlink -f releases/current.jar)"
+  ln -sfn "$OLD_TARGET" releases/previous.jar
+fi
+ln -sfn "releases/app-$STAMP.jar" releases/current.jar
+
+# Run the application
+echo "[STEP-6] Starting: java -jar \"releases/current.jar\" --spring.profiles.active=$PROFILE"
 echo "Press Ctrl+C to stop the application."
-exec java -jar "$JAR_PATH" --spring-profiles.active="$PROFILE"
+exec java -jar releases/current.jar --spring-profiles.active="$PROFILE"
