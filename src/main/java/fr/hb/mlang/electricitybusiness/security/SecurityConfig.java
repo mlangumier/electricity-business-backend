c7c3473280ca42bc4c446e71e49c1e4a92eb8345
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-10-02 18:39:13 +0200

    Feat: added JWT & authentication config

--- src/main/java/fr/hb/mlang/electricitybusiness/security/SecurityConfig.java
+++ src/main/java/fr/hb/mlang/electricitybusiness/security/SecurityConfig.java
@@ -1,37 +1,69 @@
 package fr.hb.mlang.electricitybusiness.security;
 
+import fr.hb.mlang.electricitybusiness.config.AppProperties;
 import fr.hb.mlang.electricitybusiness.security.jwt.JwtAuthenticationFilter;
+import java.time.Duration;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  private final AppProperties appProperties;
   private final JwtAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authProvider;
 
-  public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) {
+  public SecurityConfig(
+      JwtAuthenticationFilter jwtAuthFilter,
+      AppProperties appProperties,
+      AuthenticationProvider authProvider
+  ) {
+    this.appProperties = appProperties;
     this.jwtAuthFilter = jwtAuthFilter;
+    this.authProvider = authProvider;
   }
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/api/v1/me/**").authenticated()
+            //TODO: Add requestMatchers for specific HttpMethod + routes
             .anyRequest().permitAll()
         )
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .authenticationProvider(authProvider)
     ;
 
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of(this.appProperties.baseUrl()));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+    config.setMaxAge(Duration.ofHours(1));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
   }
 }
