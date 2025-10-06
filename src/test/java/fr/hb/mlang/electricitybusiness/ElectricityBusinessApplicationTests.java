b79607a3ee9fe01f3aec534e4e835630ac9f447c
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-10-06 17:57:36 +0200

    Setup test container & registration tests (1)

--- src/test/java/fr/hb/mlang/electricitybusiness/ElectricityBusinessApplicationTests.java
+++ src/test/java/fr/hb/mlang/electricitybusiness/ElectricityBusinessApplicationTests.java
@@ -1,13 +1,17 @@
 package fr.hb.mlang.electricitybusiness;
 
+import fr.hb.mlang.electricitybusiness.config.DatabaseConfigTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ElectricityBusinessApplicationTests extends DatabaseConfigTests {
 
   @Test
   void contextLoads() {
+    System.out.println("Context loaded!");
   }
 
 }
