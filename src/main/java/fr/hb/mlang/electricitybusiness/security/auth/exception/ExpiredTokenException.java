baa2799342d32a0302b80f6a16e698863c1ce69e
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-10-26 15:30:42 +0100

    Test(verify): finished setting up tests for email verification

--- src/main/java/fr/hb/mlang/electricitybusiness/security/auth/exception/ExpiredTokenException.java
+++
@@ -1,8 +0,0 @@
-package fr.hb.mlang.electricitybusiness.security.auth.exception;
-
-public class ExpiredTokenException extends RuntimeException {
-
-  public ExpiredTokenException(String message) {
-    super(message);
-  }
-}
